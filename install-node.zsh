e8c06d9291f2665b0d6317393bed3bd0ee0ab71d
Andy Nadal <andy@nadalapps.com>
2022-02-03 09:32:19 -0600

    Added yarn to npm global packages

--- install-node.zsh
+++ install-node.zsh
@@ -17,6 +17,7 @@ fi
 npm install --global typescript
 npm install --global json-server
 npm install --global trash-cli
+npm install --global yarn
 
 echo "Global NPM packages installed"
 npm list --global --depth=0
